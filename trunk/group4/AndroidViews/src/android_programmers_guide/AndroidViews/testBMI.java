7aaa5cae19a7ba8af4161ab87ae81b32ccde2fdd
xpress2pavan <xpress2pavan@3be5e824-b799-11de-92a3-373a1addbe3b>
2009-11-04 05:08:14 +0000

    second commit added some xml stufff

--- trunk/group4/AndroidViews/src/android_programmers_guide/AndroidViews/testBMI.java
+++ trunk/group4/AndroidViews/src/android_programmers_guide/AndroidViews/testBMI.java
@@ -5,9 +5,19 @@ import android.view.View;
 import android.widget.*;
 import android.graphics.Color;
 public class testBMI extends Activity {
+	
+	public EditText txtheight;
+	public EditText txtweight;
+	public EditText txtTipOther;
+	public RadioGroup rdoGroupTips;
+	public Button btnCalculate;
+	public Button btnReset;
+	public TextView txtbmi;
+	public TextView txtmsg;
+
 	@Override
-	public void onCreate(Bundle icicle) {
-	super.onCreate(icicle);
+	public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
 	setContentView(R.layout.testbmi);
 	Button okButton = (Button) findViewById(R.id.submit);
     okButton.setOnClickListener(okOnClickListener);
@@ -18,18 +28,24 @@ private Button.OnClickListener okOnClickListener =
           new Button.OnClickListener(){
     @Override
     public void onClick(View v) {
-        EditText edit_height = (EditText)
-             findViewById(R.id.height);
-        EditText edit_weight = (EditText)
-        findViewById(R.id.weight);
-        EditText edit_age = (EditText)
-        findViewById(R.id.age);
-        //int value=edit_height./edit_weight;
-        TextView bmi=(TextView) findViewById(R.id.bmi);
-        //bmi.setText(value);
-        /*CharSequence edit_text_value =
-             edit_text.getText();
-        setTitle("Hello:"+edit_text_value);*/
+   txtheight = (EditText)findViewById(R.id.height);
+   txtweight = (EditText)findViewById(R.id.weight);
+  txtbmi=(TextView) findViewById(R.id.bmi);
+  txtmsg=(TextView) findViewById(R.id.msg);
+  Double height = Double.parseDouble(txtheight.getText().toString());
+   Double weight = Double.parseDouble(txtweight.getText().toString());
+  Double bmi = weight/(height*height);
+  txtbmi.setText(bmi.toString());  
+ if(bmi<=18.5)
+	 txtmsg.setText("Underweight");
+ else if(bmi> 18.5 && bmi < 24.9)
+	 txtmsg.setText("Normal weight");
+ else if (bmi > 25 && bmi < 29.9)
+	 txtmsg.setText("Overweight");
+ else 
+	 txtmsg.setText("Obese");
+  	  
+	 
     }
 };
 
