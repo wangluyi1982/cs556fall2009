c1ec73ceb3887719ff903e41bd97dcaf5f6b1062
xpress2pavan <xpress2pavan@3be5e824-b799-11de-92a3-373a1addbe3b>
2009-11-27 23:17:47 +0000

    added statistics and help

---
+++ trunk/group4/AndroidViews/src/android_programmers_guide/AndroidViews/teststats.java
@@ -0,0 +1,14 @@
+package android_programmers_guide.AndroidViews;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+public class teststats extends Activity {
+/** Called when the Activity is first created. */
+@Override
+public void onCreate(Bundle icicle) {
+super.onCreate(icicle);
+setContentView(R.layout.teststats);
+/** This is our Test Activity
+All code goes below */
+}
+}
