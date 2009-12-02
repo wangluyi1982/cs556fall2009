e8ef14d838f425584b4b933d6aa1e8e7b15c7650
richardsjohnt <richardsjohnt@3be5e824-b799-11de-92a3-373a1addbe3b>
2009-12-02 01:26:07 +0000

    Fixed Access class. Should work now.

--- trunk/group1/ClickAccess/src/app/clickaccess/Access.java
+++ trunk/group1/ClickAccess/src/app/clickaccess/Access.java
@@ -9,41 +9,28 @@ import android.webkit.WebViewClient;
 
 
 public class Access extends Activity {
-	
-	    /** Called when the activity is first created. */
 		WebView webview;
-		//private Long mRowId;
-		private String url;
+		String defaultUrl = "http://www.google.com";
 		
-		private class AccessWebViewClient extends WebViewClient {
-	        @Override
-	        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	            view.loadUrl(url);
-	            return true;
-	        }
-	    }
-		
+		/** Called when the activity is first created. */
 	    @Override
-	    
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        //setContentView(R.layout.access_view);
-	        //webview = (WebView) findViewById(R.id.webview);
-	        //webview.setWebViewClient(new AccessWebViewClient());
-	        //webview.getSettings().setJavaScriptEnabled(true);
-	        //webview.loadUrl("http://www.google.com");
+	        setContentView(R.layout.access_view);
 	        
+	        String url = defaultUrl;
 	        Bundle extras = getIntent().getExtras();
 	        if (extras != null) {
 	        	url = extras.getString(WebinfoDbAdapter.KEY_URL);
 	        }
 	    
 		    webview = (WebView) findViewById(R.id.webview);
-	        webview.setWebViewClient(new WebViewClient());
+	        webview.setWebViewClient(new AccessWebViewClient());
 	        webview.getSettings().setJavaScriptEnabled(true);
 	        webview.loadUrl(url);
 		}
 	    
+	    /* Enables the use of the back key. */
 	    @Override
 	    public boolean onKeyDown(int keyCode, KeyEvent event) {
 	        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
@@ -51,5 +38,14 @@ public class Access extends Activity {
 	            return true;
 	        }
 	        return super.onKeyDown(keyCode, event);
+	    }
+	    
+	    /* This class disables the passing of URLs to the default Android browser. */
+		private class AccessWebViewClient extends WebViewClient {
+	        @Override
+	        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	            view.loadUrl(url);
+	            return true;
+	        }
 	    }
 	}
